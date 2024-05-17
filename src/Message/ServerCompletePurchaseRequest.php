079f2fc215196b8fae25415221956d01c3b7ebf4
Gabriel Solomon <hello@gabrielsolomon.ro>
2024-05-17 12:54:03 +0300

    fix: complete purchase

--- src/Message/ServerCompletePurchaseRequest.php
+++ src/Message/ServerCompletePurchaseRequest.php
@@ -2,6 +2,7 @@
 
 namespace Paytic\Payments\Btipay\Message;
 
+use Lcobucci\JWT\Token;
 use Paytic\Omnipay\Btipay\Message\ServerCompletePurchaseRequest as AbstractServerCompletePurchaseRequest;
 
 /**
@@ -11,4 +12,18 @@ use Paytic\Omnipay\Btipay\Message\ServerCompletePurchaseRequest as AbstractServe
 class ServerCompletePurchaseRequest extends AbstractServerCompletePurchaseRequest
 {
     use Traits\CompletePurchaseRequestTrait;
+    protected function validateToken(Token $token)
+    {
+        if ($this->validateModel()) {
+            $model = $this->getModel();
+            $this->updateParametersFromPurchase($model);
+        }
+        parent::validateToken($token);
+    }
+
+    protected function getModelIdFromRequest()
+    {
+        $data = $this->getDecodedData();
+        return $data['orderNumber'] ?? null;
+    }
 }
